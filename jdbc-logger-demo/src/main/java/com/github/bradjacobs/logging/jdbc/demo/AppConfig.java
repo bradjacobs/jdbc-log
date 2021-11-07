eced431c7f824ddd961314f853ccf76df904224c
bradjacobs <bradjacobs@gmail.com>
2021-11-06 21:08:56 -0700

    updates to example of setting log dataSource

--- jdbc-logger-demo/src/main/java/com/github/bradjacobs/logging/jdbc/demo/AppConfig.java
+++ jdbc-logger-demo/src/main/java/com/github/bradjacobs/logging/jdbc/demo/AppConfig.java
@@ -2,6 +2,7 @@ package com.github.bradjacobs.logging.jdbc.demo;
 
 import com.github.bradjacobs.logging.jdbc.LoggingDataSource;
 import com.github.bradjacobs.logging.jdbc.listeners.LoggingListener;
+import com.github.bradjacobs.logging.jdbc.listeners.Slf4jLoggingListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,13 +38,23 @@ public class AppConfig
             return innerDataSource;
         }
 
-        // simple sql log listener.  will log all SQL statement at 'info' level.
-        LoggingListener loggingListener = sql -> {
+        // example 1
+        //   'custom' sql log listener.  will log all SQL statement at 'info' level.
+        LoggingListener loggingListenerInfo = sql -> {
             if (logger.isInfoEnabled()) {
                 logger.info(sql);
             }
         };
 
-        return new LoggingDataSource(innerDataSource, loggingListener);
+        // example 2
+        //   pre-built default logListener for slf4j
+        LoggingListener loggingListener = new Slf4jLoggingListener(logger);
+
+
+        // extra convenience constructor to pass in logger directly  (behaves as example 2)
+        return new LoggingDataSource(innerDataSource, logger);
+
+
+        //return new LoggingDataSource(innerDataSource, loggingListener);
     }
 }
